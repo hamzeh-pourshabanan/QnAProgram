2f54a1e9eea8f92454a8861ca7754b600dfc20b0
hamzeh-pourshabanan <hamzeh.p89@gmail.com>
2025-04-04 18:18:26 +0200

    Implement Ask question command

--- src/main/java/AskQuestionCommand.java
+++ src/main/java/AskQuestionCommand.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class AskQuestionCommand implements Command{
     @Override
     public void execute(Scanner scanner, Map<String, List<String>> knowledgeBase) {
+        System.out.print("Enter your question: ");
+        String question = scanner.nextLine().trim();
 
+        if (knowledgeBase.containsKey(question)) {
+            knowledgeBase.get(question).forEach(System.out::println);
+        } else {
+            System.out.println("The answer to life, universe and everything is 42");
+        }
     }
 
     @Override
