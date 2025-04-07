ae17a84e97be67efab9b0ae8448657047f921310
hamzeh-pourshabanan <hamzeh.p89@gmail.com>
2025-04-07 09:28:24 +0200

    cleanup: delete unused constant

--- src/main/java/qa/common/Constants.java
+++ src/main/java/qa/common/Constants.java
@@ -3,5 +3,4 @@ package qa.common;
 public class Constants {
     public static final String UNIVERSE_RESPONSE = "The answer to life, universe and everything is 42";
     public static final int MAX_LENGTH = 255;
-    public static final String MAX_LENGTH_WARNING = "Question exceeds maximum length of 255 characters.";
 }
