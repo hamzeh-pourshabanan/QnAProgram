1307c057fcab9295f206099581c54500f5c184d3
hamzeh-pourshabanan <hamzeh.p89@gmail.com>
2025-04-07 07:33:49 +0200

    Create QAService implementing ImputPort

---
+++ src/main/java/qa/domain/service/QAService.java
@@ -0,0 +1,31 @@
+package qa.domain.service;
+
+import qa.domain.model.Answer;
+import qa.domain.model.Question;
+import qa.domain.ports.InputPort;
+import qa.domain.ports.QuestionRepository;
+
+import java.util.List;
+
+public class QAService implements InputPort {
+    private static final Answer DEFAULT_ANSWER =
+            new Answer("the answer to life, universe and everything is 42");
+
+    private final QuestionRepository repository;
+
+    public QAService(QuestionRepository repository) {
+        this.repository = repository;
+    }
+    @Override
+    public List<Answer> askQuestion(Question question) {
+        if (repository.exists(question)) {
+            return repository.findByQuestion(question);
+        }
+        return List.of(DEFAULT_ANSWER);
+    }
+
+    @Override
+    public void addQuestion(Question question, List<Answer> answers) {
+        repository.save(question, answers);
+    }
+}
